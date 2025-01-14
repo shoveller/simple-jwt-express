d241c48286eaca74d74f74bcc918489154feaae3
cinos81 <cinos81@gmail.com>
2025-01-15 06:39:28 +0900

    feat: 토큰 검증 기능 추가

--- tokens/verify.ts
+++ tokens/verify.ts
@@ -9,9 +9,14 @@ export const verifyToken = (token: string) => {
 };
 
 if (require.main === module) {
-  const { accessToken, refreshToken } = generateTokens("test");
+  const { accessToken, refreshToken } = generateTokens("사용자이름");
 
+
+  // iss (issuer): 토큰 발급자
+  // sub (subject): 토큰 제목 (일반적으로 사용자 식별자)
+  // iat (issued at): 토큰 발급 시간 (Unix 시간)
+  // exp (expiration time): 토큰 만료 시간 (Unix 시간)
   console.log(verifyToken(accessToken));
   console.log(verifyToken(refreshToken));
-  //   console.log(verifyToken("검증에 실패하는 토큰은 예외를 발생시킨다"));
+  // console.log(verifyToken("전혀 엉뚱한 값"));
 }
